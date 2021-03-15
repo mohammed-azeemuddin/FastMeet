fa6ec027c625490ef04f758f3bdf63a4ba3634d7
mohammed-azeemuddin <mohammedazeemuddin98@gmail.com>
2021-03-15 14:24:30 +0530

    added gitignore

--- user_details.py
+++
@@ -1,5 +0,0 @@
-myurl = "https://infosys.webex.com"
-meet_pass = 1843934431
-user_password = 'OSFP_Feb23'
-my_full_name = "1117053-mohammed.azeemuddin@infosys.com"
-email_id = "mohammed.azeemuddin@infosys.com"
